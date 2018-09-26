9f8f85c89eab53d881efb2d603b299a52490b2c3
supacybertek <supacybertek@gmail.com>
2018-09-26 13:48:54 -0400

    5th method

--- src/testing/SupaTest_v01.java
+++ src/testing/SupaTest_v01.java
@@ -7,10 +7,12 @@ public class SupaTest_v01 {
 		System.out.println("1");
 		System.out.println("2");
 		System.out.println("3");
+		System.out.println("5");
 //=======
 		System.out.println("1st");
 		System.out.println("2nd");
 		System.out.println("3rd");
+		System.out.println("5th");
 //>>>>>>> refs/heads/Sidney_beta_01
 	}
 
