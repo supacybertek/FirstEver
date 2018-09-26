f4cc6764e4a2a4f79f1fe2d220a0982961cf97c7
Sidney <supacybertek@gmail.com>
2018-09-25 20:39:10 -0400

    Sidney new update method

--- src/testing/TestV_01.java
+++ src/testing/TestV_01.java
@@ -6,7 +6,7 @@ public class TestV_01 {
 		System.out.println("CyberWorld");
 		System.out.println("Hola");
 		System.out.println("update");
-		System.out.println("new branch");
+		//System.out.println("new branch");
 	}
 	
 }
