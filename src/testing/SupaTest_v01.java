41616e7977f5e42e584e2054d9d1c9688a966605
supacybertek <supacybertek@gmail.com>
2018-09-26 15:03:58 -0400

    conflict fix

--- src/testing/SupaTest_v01.java
+++ src/testing/SupaTest_v01.java
@@ -7,21 +7,21 @@ public class SupaTest_v01 {
 		System.out.println("1");
 		System.out.println("2");
 		System.out.println("3");
-<<<<<<< HEAD
+
 		System.out.println("5");
-=======
+
 		System.out.println("4");
->>>>>>> refs/heads/Sidney_beta_01
-//=======
+
+
 		System.out.println("1st");
 		System.out.println("2nd");
 		System.out.println("3rd");
-<<<<<<< HEAD
+
 		System.out.println("5th");
-=======
+
 		System.out.println("4th");
->>>>>>> refs/heads/Sidney_beta_01
-//>>>>>>> refs/heads/Sidney_beta_01
+
+
 	}
 
 }
