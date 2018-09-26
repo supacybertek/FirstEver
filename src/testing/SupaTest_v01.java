95a64c0952fca449e65e9581a7807c0c0bc4b03e
supacybertek <supacybertek@gmail.com>
2018-09-26 13:30:48 -0400

    change

--- src/testing/SupaTest_v01.java
+++ src/testing/SupaTest_v01.java
@@ -3,7 +3,7 @@ package testing;
 public class SupaTest_v01 {
 
 	public static void main(String[] args) {
-		System.out.println("1st");
+		System.out.println("1");
 		System.out.println("2");
 		System.out.println("3");
 	}
